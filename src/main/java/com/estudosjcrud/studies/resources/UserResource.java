8a7178067c4c9bf4be428413de136d1fef918a09
Matheus Rosa <matheus_a_rosa@hotmail.com>
2023-07-09 22:56:44 -0300

    User entity and resource

---
+++ src/main/java/com/estudosjcrud/studies/resources/UserResource.java
@@ -0,0 +1,19 @@
+package com.estudosjcrud.studies.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.estudosjcrud.studies.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+	
+	@GetMapping
+	public ResponseEntity<User> findAll(){
+		User u = new User(1L, "Maria", "maria@gmail.com", "98765432", "123456");
+		return ResponseEntity.ok().body(u);
+	}
+}
